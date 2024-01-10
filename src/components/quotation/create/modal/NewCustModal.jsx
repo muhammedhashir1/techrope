6fe6ad333f9e0d7b61ecb37b9e5cda0206fb805b
muhammed hashir <hashiraachi818@gmail.com>
2024-01-10 23:48:47 +0530

    design working progress

--- src/components/quotation/create/modal/NewCustModal.jsx
+++ src/components/quotation/create/modal/NewCustModal.jsx
@@ -8,7 +8,7 @@ const NewCustModal = ({ closeModal }) => {
     <div className={styles.modalBackdrop} onClick={closeModal}>
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
         <header className={styles.modalContent_head}>
-          <div className={styles.modalContent_title}>New Customer Info</div>
+          <div className={styles.modalContent_title}>Create Customer</div>
           <div className={styles.closeIcon} onClick={closeModal}>
             <IoClose />
           </div>
@@ -16,31 +16,35 @@ const NewCustModal = ({ closeModal }) => {
         <div className={styles.inputAreaMain}>
           <div className={styles.ledgerTitel}>
             <label htmlFor="">Ledger Name</label>
-            <input type="text" name="" id="" />
+            <input type="text" name="" id="" className={styles.ledgerInput} />
             <label htmlFor="">Ledger Code</label>
-            <input type="text" name="" id="" />
+            <input type="text" name="" id="" className={styles.ledgerInput} />
             <label htmlFor="">Ledger Local Name</label>
-            <input type="text" name="" id="" />
+            <input type="text" name="" id="" className={styles.ledgerInput} />
             <label htmlFor="">Account Group</label>
-            <select name="" id="">
+            <select name="" id="" className={styles.acntGrp}>
               <option value="">SUNDRY DEBTORS</option>
             </select>
             <label htmlFor="">Opening Balance</label>
             <div className={styles.openingBalance}>
-              <select name="" id="">
+              <select name="" id="" className={styles.selectDebCrd}>
                 <option value="">Debit</option>
                 <option value="">Credit</option>
               </select>
-              <input type="text" name="" id="" placeholder="Opening Balance" />
+              <input type="text" name="" id="" placeholder="Opening Balance" className={styles.inputOpnBal} />
             </div>
             <div className={styles.subledgerBillWise}>
               <div className={styles.subledger}>
-                <input type="checkbox" name="" id="" />
-                <label htmlFor="">Sub Ledger</label>
+                <input type="checkbox" name="" id="" className={styles.subledgerInput} />
+                <label htmlFor="" className={styles.subledgerLabel}>
+                  Sub Ledger
+                </label>
               </div>
               <div className={styles.BillWise}>
-                <input type="checkbox" name="" id="" />
-                <label htmlFor="">Maintain BillWise Details</label>
+                <input type="checkbox" name="" id="" className={styles.billWiseInput} />
+                <label htmlFor="" className={styles.billWiseLabel}>
+                  Maintain BillWise Details
+                </label>
               </div>
             </div>
             <div className={styles.twoButtonsMain}>
@@ -48,11 +52,11 @@ const NewCustModal = ({ closeModal }) => {
               <div className={styles.additionalInfo}>Additional Info</div>
             </div>
             <div className={styles.addressMain}>
-              <span className={styles.address}>Address</span>
+              <div className={styles.address}>Address</div>
               <textarea name="" id="" cols="30" rows="10" placeholder="Address"></textarea>
             </div>
             <div className={styles.localAddressMain}>
-              <span className={styles.localAddress}>Local Address</span>
+              <div className={styles.localAddress}>Local Address</div>
               <textarea name="" id="" cols="30" rows="10" placeholder="Address"></textarea>
             </div>
 
@@ -65,15 +69,19 @@ const NewCustModal = ({ closeModal }) => {
               <input type="text" className={styles.mobileInput} />
               <div className={styles.localAddress}>Email</div>
               <input type="text" className={styles.emailInput} />
-              <div className={styles.localAddress}>Zip Code</div>
-              <input type="text" className={styles.zipInput} />
-              <div className={styles.localAddress}>Zip Code</div>
-              <input type="text" className={styles.zipInput} />
+              <div className={styles.localAddress}>Country</div>
+              <input type="text" className={styles.countryInput} />
+              <div className={styles.localAddress}>State</div>
+              <input type="text" className={styles.stateInput} />
             </div>
 
             <label htmlFor=""></label>
           </div>
         </div>
+        <footer className={styles.moreInfoModal_footer}>
+          <div className={styles.footer_btn}>Create</div>
+          <div className={styles.footer_btn}>Cancel</div>
+        </footer>
       </div>
     </div>
   );
